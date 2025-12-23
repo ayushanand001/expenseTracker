a4ad755d56af712b9f81c7c22b6346c709c47d0d
Ayush Anand <ayushanand10521@gmail.com>
2025-12-23 06:28:10 +0530

    ADD delete and filter operation

--- routes/expenses.js
+++ routes/expenses.js
@@ -10,9 +10,17 @@ router.get("/", (req, res) => {
 
 //get request
 router.get("/dashboard", ensureAuthenticated, async (req, res) => {
+  const currentFilter = req.query.filter || "all";
+  let str = "";
+
+  if (currentFilter == "all") str = "";
+  else if (currentFilter === "week")
+    str = "AND created_at > CURRENT_DATE - INTERVAL '7 days'";
+  else if (currentFilter === "month")
+    str = "AND created_at > CURRENT_DATE - INTERVAL '30 days'";
   try {
     const data = await db.query(
-      "SELECT * FROM expenses WHERE user_id=$1 ORDER BY created_at DESC",
+      `SELECT * FROM expenses WHERE user_id=$1 ${str} ORDER BY created_at DESC`,
       [req.user.id]
     );
 
@@ -27,9 +35,11 @@ router.get("/dashboard", ensureAuthenticated, async (req, res) => {
       expenses: expenses,
       totalSpent: totalamount,
       user: req.user,
+      currentFilter: currentFilter,
     });
   } catch (err) {
     console.log(err);
+    res.status(500).send("Error loading dashboard");
   }
 });
 
@@ -40,6 +50,20 @@ router.post("/expenses/add", ensureAuthenticated, async (req, res) => {
       "INSERT INTO expenses(description, amount, category, user_id) VALUES($1,$2,$3,$4)",
       [req.body.description, req.body.amount, req.body.category, req.user.id]
     );
+    res.redirect("/dashboard");
+  } catch (err) {
+    console.log(err);
+    res.redirect("/dashboard");
+  }
+});
+
+router.get("/expenses/delete/:id", ensureAuthenticated, async (req, res) => {
+  try {
+    const id = req.params.id;
+    await db.query("DELETE FROM expenses WHERE id=$1 AND user_id=$2", [
+      id,
+      req.user.id,
+    ]);
     res.redirect("/dashboard");
   } catch (err) {
     console.log(err);
